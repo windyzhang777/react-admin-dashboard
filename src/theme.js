c459bcd2e3814b43e33715e9645812638099e643
Windy Zhang <windy.zhang777@hotmail.com>
2023-07-14 20:31:53 -0500

    fixed colorMode

--- src/theme.js
+++ src/theme.js
@@ -196,14 +196,13 @@ export const ColorModeContext = createContext({
 export const useMode = () => {
   const [mode, setMode] = useState("dark");
   const colorMode = useMemo(
-    () => (
-      {
-        toggleColorMode: () =>
-          setMode((prev) => (prev === "light" ? "dark" : "light")),
-      },
-      []
-    )
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
   );
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return [theme, colorMode];
 };
