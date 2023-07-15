9354935234c54106bf5584013f62d4e36fc9d089
Windy Zhang <windy.zhang777@hotmail.com>
2023-07-14 20:32:53 -0500

    added IconButton and toggle mode func in topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -2,13 +2,15 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import { Box, InputBase, useTheme } from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
+import InputBase from "@mui/material/InputBase";
 import React from "react";
 import UseColorModeContext from "../../hooks/useColorModeContext";
 import { tokens } from "../../theme";
 
-export default function Topbar({}) {
+export default function Topbar() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const { toggleColorMode } = UseColorModeContext();
@@ -16,22 +18,35 @@ export default function Topbar({}) {
   return (
     <Box display={"flex"} justifyContent={"space-between"} p={2}>
       {/* SEARCH BAR */}
-      <Box display={"flex"} bgcolor={colors.primary[400]}>
-        <InputBase />
+      <Box display={"flex"} bgcolor={colors.primary[400]} borderRadius={1}>
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
       </Box>
-      <Box display={"flex"} gap={1}>
+
+      {/* ICONS */}
+      <Box display={"flex"}>
         {/* DARK/LIGHT MODE */}
-        {theme.palette.mode === "dark" ? (
-          <DarkModeOutlinedIcon />
-        ) : (
-          <LightModeOutlinedIcon />
-        )}
+        <IconButton onClick={toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
         {/* NOTIFICATION */}
-        <NotificationsOutlinedIcon />
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
         {/* SETTINGS */}
-        <SettingsOutlinedIcon />
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
         {/* USER */}
-        <PersonOutlinedIcon />
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
       </Box>
     </Box>
   );
