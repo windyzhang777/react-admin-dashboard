5aaa47a56c98c0d935cd800fbe57dbb97759d8ed
Windy Zhang <windy.zhang777@hotmail.com>
2023-07-16 23:09:42 -0500

    added routes for contacts, invoices, and form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
+import Contacts from "./scenes/contacts";
 import Dashboard from "./scenes/dashboard";
+import ProfileForm from "./scenes/form";
 import Sidebar from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
+import Invoices from "./scenes/invoices";
 import Team from "./scenes/team";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -20,6 +23,9 @@ export default function App() {
               <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<ProfileForm />} />
               </Routes>
             </main>
           </div>
